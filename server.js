c802a347fa7093a7b2e19ecbef4f9fba8e531cd5
Brains <n-orr@onu.edu>
2016-04-19 11:57:21 -0400

    Under Construction

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 http.createServer(function(req, res) {
     console.log('Got request for ' + req.url);
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('<h1>Hello Alaina!</h1>');
+    res.end('<h1>Alaina and Noah wedding website under construction.</h1>');
 }).listen(process.env.PORT);
